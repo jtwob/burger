e08b5d1d8a36ab3f454ce4444968af3bcc0870a4
jtwob <32282322+jtwob@users.noreply.github.com>
2021-01-12 12:13:21 -0800

    NEW: Database setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Portabello Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Impossible Burger');
