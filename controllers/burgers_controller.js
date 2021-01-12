446b48395882ae9b19e3b720872970211ee8b4b1
jtwob <32282322+jtwob@users.noreply.github.com>
2021-01-12 14:03:00 -0800

    UPDATE: router in burgers_controller.js basic functionality

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,26 @@ const express = require('express');
 const burger = require('../models/burger');
 
 const router = express.Router();
+
+router.get("/", (req, res)=>{
+    burger.selectAll((results)=>{
+        let obj = {
+            burgers: results
+        };
+        res.render("index", obj);
+    });
+});
+
+router.post("/burgers", (req, res)=>{
+    burger.insertOne(req.body.burger_name, ()=>{
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/:id", (req, res)=>{
+    burger.updateOne(req.params.id, ()=>{
+        res.redirect("/");
+    });
+});
+
+module.exports = router;
