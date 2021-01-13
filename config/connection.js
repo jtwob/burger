743689ba354e6cf6064212aa8ef083b4815609a6
jtwob <32282322+jtwob@users.noreply.github.com>
2021-01-12 19:39:31 -0800

    UPDATE: code to put app on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql');
-
+var connection;
 // Set up our connection information
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db',
+  });
+};
+
 
 // Connect to the database
 connection.connect((err) => {
