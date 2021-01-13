45ddcae5fca16b6094262b08890928359cca0905
jtwob <32282322+jtwob@users.noreply.github.com>
2021-01-12 20:07:14 -0800

    UPDATE: Fixing schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 
 CREATE TABLE burgers(
     id INT AUTO_INCREMENT,
-    burger_name VARCHAR(100),
+    burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
